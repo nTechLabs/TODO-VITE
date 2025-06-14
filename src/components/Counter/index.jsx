9bf3e27cb944b6637c2a706ea4f5dba9891a681f
vjay7394 <zhl22pa@gmail.com>
2025-06-14 23:00:14 +0900

    MUI적용함.

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { counterSliceActions } from "../../store/counterSlice";
+import { Button, Box } from "@mui/material";
 
 function Counter() {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <button onClick={() => dispatch(counterSliceActions.down(1))}>-</button>
-      {count}
-      <button onClick={() => dispatch(counterSliceActions.up(1))}>
+    <Box display="flex" alignItems="center" gap={2}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => dispatch(counterSliceActions.down(1))}
+      >
+        -
+      </Button>
+      <span style={{ minWidth: 32, textAlign: "center" }}>{count}</span>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => dispatch(counterSliceActions.up(1))}
+      >
         +
-      </button>{" "}
-    </div>
+      </Button>
+    </Box>
   );
 }
 
