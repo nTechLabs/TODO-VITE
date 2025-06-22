87076e31c6428c5229d54db5d33a443ab9143eba
droi <droi.7394@gmail.com>
2025-06-22 18:26:52 +0900

    card css분리함.

---
+++ src/values/cardContents.js
@@ -0,0 +1,44 @@
+const cardContents = [
+  {
+    title: 'Default size card 1',
+    extra: <a href="#">More</a>,
+    style: { width: 300 },
+    spaceName: 'SpaceName1',
+    taskName: 'TaskName1',
+    DateTime: '2025-06-22 10:00',
+  },
+  {
+    title: 'Default size card 2',
+    extra: <a href="#">More</a>,
+    style: { width: 300 },
+    spaceName: 'SpaceName2',
+    taskName: 'TaskName2',
+    DateTime: '2025-06-22 11:00',
+  },
+  {
+    title: 'Default size card 3',
+    extra: <a href="#">More</a>,
+    style: { width: 300 },
+    spaceName: 'SpaceName3',
+    taskName: 'TaskName3',
+    DateTime: '2025-06-22 12:00',
+  },
+  {
+    title: 'Default size card 4',
+    extra: <a href="#">More</a>,
+    style: { width: 300 },
+    spaceName: 'SpaceName4',
+    taskName: 'TaskName4',
+    DateTime: '2025-06-22 13:00',
+  },
+  {
+    title: 'Default size card 5',
+    extra: <a href="#">More</a>,
+    style: { width: 300 },
+    spaceName: 'SpaceName5',
+    taskName: 'TaskName5',
+    DateTime: '2025-06-22 14:00',
+  },
+];
+
+export default cardContents;
