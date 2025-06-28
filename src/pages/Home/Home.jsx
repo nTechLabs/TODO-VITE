b4f2cbd59bb79ac58304f87065eb3cf50ba1ce43
vjay7394 <zhl22pa@gmail.com>
2025-06-28 19:22:03 +0900

    ...

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,16 +17,16 @@ const Home = () => {
             onClick={() => navigate('/home/CardDetail')}
             style={{ cursor: 'pointer' }}
           >
-            <div style={{ fontWeight: 600, fontSize: 16, display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', marginBottom: 8 }}>
-              <span>{card.title}</span>
-              <Badge count={card.count} style={{ backgroundColor: '#faad14' }} />
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', marginBottom: 8, flexWrap: 'wrap' }}>
+              <span style={{ fontWeight: 600, fontSize: 16, wordBreak: 'break-all', flex: '1 1 120px', minWidth: 0 }}>{card.title}</span>
+              <Badge count={card.count} style={{ backgroundColor: '#faad14', marginLeft: 8 }} />
             </div>
             <div style={{ marginBottom: 4 }}>
               <span style={{ background: '#f5f5f5', borderRadius: 6, padding: '2px 10px', fontWeight: 500, color: '#555', fontSize: 14, display: 'inline-block', marginBottom: 2 }}>{card.spaceName}</span>
               <br />
               <span style={{ fontWeight: 400, color: '#222', fontSize: 15 }}>{card.taskName}</span>
             </div>
-            <div style={{ textAlign: 'right', color: '#888', fontSize: 13, marginTop: 8 }}>
+            <div style={{ textAlign: 'center', color: '#888', fontSize: 13, marginTop: 8, wordBreak: 'break-all', whiteSpace: 'normal', width: '100%', overflowWrap: 'anywhere' }}>
               {card.DateTime}
             </div>
           </Card>
