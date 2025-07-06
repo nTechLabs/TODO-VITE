6cd6a5d85316fba82c236fca2ce46dae13fd1990
droi <droi.7394@gmail.com>
2025-07-06 13:59:38 +0900

    api경로수정

--- src/pages/TodoApi/api.js
+++
@@ -1,7 +0,0 @@
-export const TODOS_API_URL = "https://jsonplaceholder.typicode.com/todos";
-
-export const fetchTodos = async () => {
-  const res = await fetch(TODOS_API_URL);
-  if (!res.ok) throw new Error("Network response was not ok");
-  return res.json();
-};
