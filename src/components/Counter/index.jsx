ef855af6a499d9d9b2431001fb9af6148e3be747
vjay7394 <zhl22pa@gmail.com>
2025-07-11 06:36:22 +0900

    ...

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -3,9 +3,27 @@ import { Button, Space } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { counterSliceActs } from "../../store/counterSlice";
 
-function aaa(){
-  console.log("aaaaa");
+function Counter() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <Space align="center">
+      <Button
+        type="primary"
+        onClick={() => dispatch(counterSliceActs.down(1))}
+      >
+        -
+      </Button>
+      <span style={{ minWidth: 32, textAlign: "center" }}>{count}</span>
+      <Button
+        type="primary"
+        onClick={() => dispatch(counterSliceActs.up(1))}
+      >
+        +
+      </Button>
+    </Space>
+  );
 }
 
-export default aaa;
-
+export default Counter;
